9995afb6fb54633b844ac299c9e7bc83b87cc12f
David A. Case <dacase1@gmail.com>
2022-09-24 22:38:31 -0400

    use NAB_matcpy to get matrix back from superimpose; latter just gets the matrix --doens't really do the transformation

--- test/program_2.c
+++ test/program_2.c
@@ -9,12 +9,14 @@ int main( int argc, char *argv[] )
 	nabout = stdout; /*default*/
 
     MOLECULE_T *m, *mr;
+    MATRIX_T  mat;
     double r;
 
     m = getpdb( "test.pdb", "" );
     mr = getpdb( "gcg10.pdb", "" );
 
-    superimpose( m, "::C1'", mr, "::C1'" );
+    NAB_matcpy( mat, superimpose( m, "::C1'", mr, "::C1'" ) );
+    transformmol( mat, m, NULL );
     putpdb( "test.sup.pdb", m, "" );
     // rmsd( m, "::C1'", mr, "::C1'", r );
     // printf( "rmsd = %8.3f\n", r );
